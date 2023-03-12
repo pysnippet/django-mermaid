2d389830df0288d441f1fa8f246fbf952e2cd289
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-12 20:44:22 +0400

    enable include_package_data and create mermaid.js before writing into it

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
-import os
-# from os.path import dirname, join
-# from urllib.request import urlretrieve
+from os.path import dirname, join
+from urllib.request import urlretrieve
 
 from setuptools import setup
 
 if __name__ == "__main__":
-    # urlretrieve(
-    #     "https://cdnjs.cloudflare.com/ajax/libs/mermaid/9.4.3/mermaid.js",
-    #     join(dirname(__file__), "src", "django_mermaid", "static", "mermaid.js")
-    # )
-    os.system("ls -la")
+    cdn = "https://cdnjs.cloudflare.com/ajax/libs/mermaid/9.4.3/mermaid.js"
+    static = join(dirname(__file__), "src", "django_mermaid", "static")
+    open(join(static, "mermaid.js"), "w").close()
+    urlretrieve(cdn, join(static, "mermaid.js"))
     setup()
