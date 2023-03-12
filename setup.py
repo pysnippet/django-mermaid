c51d5637a4211addb708eeed0483019e0e4918c8
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-12 20:24:33 +0400

    Use absolute path

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from os.path import dirname, join
 from urllib.request import urlretrieve
 
 from setuptools import setup
@@ -5,6 +6,6 @@ from setuptools import setup
 if __name__ == "__main__":
     urlretrieve(
         "https://cdnjs.cloudflare.com/ajax/libs/mermaid/9.4.3/mermaid.js",
-        "src/django_mermaid/static/mermaid.js",
+        join(dirname(__file__), "src", "django_mermaid", "static", "mermaid.js")
     )
     setup()
