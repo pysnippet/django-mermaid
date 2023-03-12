b65c584ce05d8d0a0b38cdd430077f161a451a03
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-12 20:27:13 +0400

    Comment urlretrieve

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import os
-from os.path import dirname, join
-from urllib.request import urlretrieve
+# from os.path import dirname, join
+# from urllib.request import urlretrieve
 
 from setuptools import setup
 
 if __name__ == "__main__":
-    urlretrieve(
-        "https://cdnjs.cloudflare.com/ajax/libs/mermaid/9.4.3/mermaid.js",
-        join(dirname(__file__), "src", "django_mermaid", "static", "mermaid.js")
-    )
+    # urlretrieve(
+    #     "https://cdnjs.cloudflare.com/ajax/libs/mermaid/9.4.3/mermaid.js",
+    #     join(dirname(__file__), "src", "django_mermaid", "static", "mermaid.js")
+    # )
     os.system("ls -la")
     setup()
