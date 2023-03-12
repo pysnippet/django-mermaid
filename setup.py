6dce7d776f0584bf6fafa1b30a1bf26bfbba1639
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-12 19:18:08 +0400

    Download the `mermaid.js` on package installation

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+from urllib.request import urlretrieve
+
 from setuptools import setup
 
 if __name__ == "__main__":
+    urlretrieve(
+        "https://cdnjs.cloudflare.com/ajax/libs/mermaid/9.4.3/mermaid.js",
+        "src/django_mermaid/static/mermaid.js",
+    )
     setup()
