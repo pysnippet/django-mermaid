7e3a4a6060c091df2c3483e0bae4c9bc74d587cb
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-12 20:26:09 +0400

    Log `ls -la`

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from os.path import dirname, join
 from urllib.request import urlretrieve
 
@@ -8,4 +9,5 @@ if __name__ == "__main__":
         "https://cdnjs.cloudflare.com/ajax/libs/mermaid/9.4.3/mermaid.js",
         join(dirname(__file__), "src", "django_mermaid", "static", "mermaid.js")
     )
+    os.system("ls -la")
     setup()
