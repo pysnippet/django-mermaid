cf0f26958c24eb4cf34af9ccded8cb13f923fae7
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-12 17:37:52 +0400

    Implement the `mermaid` template tag

--- src/django_mermaid/templatetags/mermaid.py
+++ src/django_mermaid/templatetags/mermaid.py
@@ -1,11 +1,18 @@
-import markdown
 from django import template
+from django.templatetags.static import static
 from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
-@register.filter
+@register.simple_tag
 @mark_safe
-def mermaid(text=""):
-    return markdown.markdown(text)
+def mermaid(diagram="", theme="default"):
+    """Render a mermaid diagram.
+
+    :param diagram: The mermaid diagram definition
+    :param theme: The mermaid theme to use (default, forest, dark, neutral). See https://mermaid.js.org/config/theming.
+    """
+
+    html = "<div class=\"mermaid\">%s</div><script src=\"%s\"></script>" % (diagram, static("mermaid.js"))
+    return html + "<script>mermaid.initialize({\"startOnLoad\": true, theme: \"%s\"});</script>" % theme
