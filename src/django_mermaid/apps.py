adc6802731e2ffe870c8190358f3e763358f5283
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-13 16:18:18 +0400

    GH-5: Handle `MERMAID_VERSION` setting to use preferred version

--- src/django_mermaid/apps.py
+++ src/django_mermaid/apps.py
@@ -1,8 +1,12 @@
 from distutils import dir_util
-from os.path import dirname, join, exists
+from os import stat
+from os.path import dirname
+from os.path import exists
+from os.path import join
 from urllib.request import urlretrieve
 
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class MermaidConfig(AppConfig):
@@ -10,8 +14,11 @@ class MermaidConfig(AppConfig):
 
     def ready(self):
         """Download mermaid.js from CDN if not already present"""
-        cdn = "https://cdnjs.cloudflare.com/ajax/libs/mermaid/9.4.3/mermaid.js"
-        static = join(dirname(__file__), "static")
-        if not exists(join(static, "mermaid.js")):
-            dir_util.create_tree(static, ["mermaid.js"])
-            urlretrieve(cdn, join(static, "mermaid.js"))
+        mermaid_version = getattr(settings, "MERMAID_VERSION", "9.4.3")
+        cdn = "https://cdnjs.cloudflare.com/ajax/libs/mermaid/%s/mermaid.js" % mermaid_version
+        static_dir = join(dirname(__file__), "static")
+        mermaid_dir = join(static_dir, "mermaid", mermaid_version)
+        if not exists(join(mermaid_dir, "mermaid.js")) or \
+                stat(join(mermaid_dir, "mermaid.js")).st_size == 0:
+            dir_util.create_tree(mermaid_dir, ["mermaid.js"])
+            urlretrieve(cdn, join(mermaid_dir, "mermaid.js"))
