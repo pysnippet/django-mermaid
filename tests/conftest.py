5f9904f59044e941839896882d6f2a9bc7166aa0
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-13 12:39:35 +0400

    Add two more test cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,13 @@ from django.conf import settings
 
 
 def pytest_configure():
-    settings.configure(INSTALLED_APPS=["django_mermaid.apps.MermaidConfig"])
+    settings.configure(
+        INSTALLED_APPS=[
+            "django_mermaid.apps.MermaidConfig"
+        ],
+        TEMPLATES=[
+            {
+                "BACKEND": "django.template.backends.django.DjangoTemplates",
+            },
+        ]
+    )
