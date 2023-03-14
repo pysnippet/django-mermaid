e19038b74a4d938d188126f6815d61178ac7961f
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-14 12:51:52 +0400

    GH-11: Add support for default theme

--- src/django_mermaid/templatetags/mermaid.py
+++ src/django_mermaid/templatetags/mermaid.py
@@ -1,21 +1,26 @@
 from django import template
+from django.conf import settings
 from django.templatetags.static import static
 from django.utils.safestring import mark_safe
 
-from . import MERMAID_VERSION
+from . import DEFAULT_THEME
+from . import DEFAULT_VERSION
 
 register = template.Library()
 
 
 @register.simple_tag
 @mark_safe
-def mermaid(diagram="", theme="default"):
+def mermaid(diagram=None, theme=None):
     """Render a mermaid diagram.
 
     :param diagram: The mermaid diagram definition
     :param theme: The mermaid theme to use (default, forest, dark, neutral). See https://mermaid.js.org/config/theming.
     """
 
-    mermaid_uri = static("mermaid/%s/mermaid.js" % MERMAID_VERSION)
-    html = "<div class=\"mermaid\">%s</div><script src=\"%s\"></script>" % (diagram, mermaid_uri)
+    version = getattr(settings, "MERMAID_VERSION", DEFAULT_VERSION)
+    theme = theme or getattr(settings, "MERMAID_THEME", DEFAULT_THEME)
+
+    mermaid_uri = static("mermaid/%s/mermaid.js" % version)
+    html = "<div class=\"mermaid\">%s</div><script src=\"%s\"></script>" % (diagram or "", mermaid_uri)
     return html + "<script>mermaid.initialize({\"startOnLoad\": true, theme: \"%s\"});</script>" % theme
