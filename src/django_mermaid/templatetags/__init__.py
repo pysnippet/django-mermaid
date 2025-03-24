7c755c18fc6438891fd04d3c4b3013533960dfee
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2025-03-24 14:28:12 +0400

    GH-20: Implement `MERMAID_USE_CDN` setting property

--- src/django_mermaid/templatetags/__init__.py
+++ src/django_mermaid/templatetags/__init__.py
@@ -1,7 +1,12 @@
+MERMAID_CDN = "https://cdnjs.cloudflare.com/ajax/libs/mermaid/%s/mermaid.min.js"
+
 DEFAULT_VERSION = "9.4.3"  # default to latest stable version
 DEFAULT_THEME = "default"  # use the mermaid 'default' theme
+DEFAULT_USE_CDN = False  # use local mermaid.js by default
 
 __all__ = [
+    "DEFAULT_USE_CDN",
     "DEFAULT_VERSION",
     "DEFAULT_THEME",
+    "MERMAID_CDN",
 ]
